4935146c448d891a4cefdbd69f13dcb8c3ca74b8
DevilRep <flaringrep@gmail.com>
2021-10-24 23:20:03 +0300

    Added simple REST for messages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { Message } from './db/entities/message';
+import { MessagesModule } from './modules/messages/messages.module';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_DATABASE,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        synchronize: true,
+        entities: [Message]
+      }),
+      MessagesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
