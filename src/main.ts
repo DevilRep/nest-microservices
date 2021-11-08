a6d5c9563f9af67d63c56c5262acfe5ea4062473
DevilRep <flaringrep@gmail.com>
2021-11-08 23:22:50 +0200

    Updated main file

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(
-      AppModule,
-      {
-          transport: Transport.TCP,
-          options: {
-              host: process.env.MICROSERVICE_SERVER_HOST,
-              port: Number(process.env.MICROSERVICE_SERVER_PORT)
-          }
-      }
-  );
-  await app.listen();
+    if (process.env.IS_MICROSERVICE && process.env.IS_MICROSERVICE === 'true') {
+        const app = await NestFactory.create(AppModule);
+        await app.listen(process.env.MICROSERVICE_SERVER_PORT);
+        return
+    }
+    const app = await NestFactory.createMicroservice(
+        AppModule,
+        {
+            transport: Transport.TCP,
+            options: {
+                host: process.env.MICROSERVICE_SERVER_HOST,
+                port: Number(process.env.MICROSERVICE_SERVER_PORT)
+            }
+        }
+    );
+    await app.listen();
 }
 bootstrap();
